a9fae06404468ee11ad8efe893bfd86d6df6f0b2
Javier Sánchez Gómez <nimrockdev@gmail.com>
2023-03-21 08:15:43 +0100

    Update getProductByID.js

--- src/helpers/getProductByID.js
+++ src/helpers/getProductByID.js
@@ -1,6 +1,6 @@
 export const getProductByID = async (word) => {
 
-    const url = `https://devarc.herokuapp.com/products/searchByID/${word}`;
+    const url = `https://devarc-production.up.railway.app/products/searchByID/${word}`;
     console.log(url)
     const resp = await fetch(url);
     let data = await resp.json();
