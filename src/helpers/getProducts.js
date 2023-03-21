0b776946a19b84c2ec6292da9bb4734107f39426
Javier Sánchez Gómez <nimrockdev@gmail.com>
2023-03-21 08:16:07 +0100

    Update getProducts.js

--- src/helpers/getProducts.js
+++ src/helpers/getProducts.js
@@ -1,6 +1,6 @@
 export const getProducts = async(word) => {
 
-    const url = `https://devarc.herokuapp.com/products/search/${word}`;    
+    const url = `https://devarc-production.up.railway.app/products/search/${word}`;    
     const resp = await fetch(url);
     let data = await resp.json();
     data = data.products;
